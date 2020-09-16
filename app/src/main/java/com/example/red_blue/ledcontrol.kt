1abf2a9e15f752e8fe28b91498ea49567cd85954
Sasinipa <s6001011621109@email.kmutnb.ac.th>
2020-09-16 21:03:31 +0700

    change seekbar.onprogress to seekbar.stopprogress in ledcontrol.kt

--- app/src/main/java/com/example/red_blue/ledcontrol.kt
+++ app/src/main/java/com/example/red_blue/ledcontrol.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.widget.AbsSeekBar
 import android.widget.ProgressBar
 import android.widget.SeekBar
+import android.widget.Toast
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_ledcontrol.*
 import kotlinx.android.synthetic.main.activity_ledmode.*
@@ -21,105 +22,113 @@ class ledcontrol : AppCompatActivity() {
 
         seekbar_photored.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, progress_red: Int, p2: Boolean) {
+                Toast.makeText(this@ledcontrol, progress_red.toString() + " %", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar) {
                 val ref = FirebaseDatabase.getInstance().getReference("/user/LED/Photo Red")
-                ref.setValue(progress_red)
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
+                ref.setValue(p0.progress)
             }
         })
 
         seekbar_green.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, progress_green: Int, p2: Boolean) {
+                Toast.makeText(this@ledcontrol, progress_green.toString() + " %", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar) {
                 val ref = FirebaseDatabase.getInstance().getReference("/user/LED/Green")
-                ref.setValue(progress_green)
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
+                ref.setValue(p0.progress)
             }
         })
 
         seekbar_moonlight.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, progress_moon: Int, p2: Boolean) {
+                Toast.makeText(this@ledcontrol, progress_moon.toString() + " %", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar) {
                 val ref = FirebaseDatabase.getInstance().getReference("/user/LED/Moon Light")
-                ref.setValue(progress_moon)
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
+                ref.setValue(p0.progress)
             }
         })
 
         seekbar_coolwhite.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, progress_cool: Int, p2: Boolean) {
+                Toast.makeText(this@ledcontrol, progress_cool.toString() + " %", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar) {
                 val ref = FirebaseDatabase.getInstance().getReference("/user/LED/Cool White")
-                ref.setValue(progress_cool)
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
+                ref.setValue(p0.progress)
             }
         })
 
         seekbar_blue.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, progress_blue: Int, p2: Boolean) {
+                Toast.makeText(this@ledcontrol, progress_blue.toString() + " %", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar) {
                 val ref = FirebaseDatabase.getInstance().getReference("/user/LED/Blue")
-                ref.setValue(progress_blue)
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
+                ref.setValue(p0.progress)
             }
         })
 
         seekbar_royalblue.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, progress_royalblue: Int, p2: Boolean) {
+                Toast.makeText(this@ledcontrol, progress_royalblue.toString() + " %", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar) {
                 val ref = FirebaseDatabase.getInstance().getReference("/user/LED/Royal Blue")
-                ref.setValue(progress_royalblue)
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
+                ref.setValue(p0.progress)
             }
         })
 
         seekbar_uv.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, progress_uv: Int, p2: Boolean) {
+                Toast.makeText(this@ledcontrol, progress_uv.toString() + " %", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar) {
                 val ref = FirebaseDatabase.getInstance().getReference("/user/LED/UV")
-                ref.setValue(progress_uv)
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
+                ref.setValue(p0.progress)
             }
         })
 
         seekbar_violet.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, progress_violet: Int, p2: Boolean) {
+                Toast.makeText(this@ledcontrol, progress_violet.toString() + " %", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar) {
                 val ref = FirebaseDatabase.getInstance().getReference("/user/LED/Violet")
-                ref.setValue(progress_violet)
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
+                ref.setValue(p0.progress)
             }
         })
 
